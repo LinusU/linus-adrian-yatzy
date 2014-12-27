fd34d2c32a3fcd5213e98a97357083fb08583649
Linus Unnebäck <linus@folkdatorn.se>
2014-12-27 13:52:55 +0100

    endgame: sort results

--- yatzy_ui_endgame.c
+++ yatzy_ui_endgame.c
@@ -29,10 +29,37 @@ const char ENDGAME[H][W] = {
   "  < < < < < < C H R I S T M A S > > > > > >  "
 };
 
+struct yatzy_ui_endgame {
+  int score;
+  char *name;
+};
+
 void yatzy_ui_endgame_show(struct yatzy_game *game) {
 
   int i;
   WINDOW *win;
+  struct yatzy_ui_endgame tmp;
+  struct yatzy_ui_endgame list[game->totalPlayers];
+
+  for (i=0; i<game->totalPlayers; i++) {
+    list[i].score = yatzy_player_total_score(game->players[i]);
+    list[i].name = game->players[i]->name;
+  }
+
+  bool sorted = false;
+  while (sorted == false) {
+    sorted = true;
+
+    for (i=1; i<game->totalPlayers; i++) {
+      if (list[i - 1].score < list[i].score) {
+        memcpy(&tmp, &list[i], sizeof(struct yatzy_ui_endgame));
+        memcpy(&list[i], &list[i - 1], sizeof(struct yatzy_ui_endgame));
+        memcpy(&list[i - 1], &tmp, sizeof(struct yatzy_ui_endgame));
+        sorted = false;
+      }
+    }
+
+  }
 
   noecho();
   curs_set(0);
@@ -48,8 +75,8 @@ void yatzy_ui_endgame_show(struct yatzy_game *game) {
   for (i=0; i<game->totalPlayers; i++) {
     int y = 7 + (i * 2);
     mvwprintw(win, y, 6, "%d.", (i + 1));
-    mvwaddnstr(win, y, 9, game->players[i]->name, 28);
-    mvwprintw(win, y, 38, "%3d", yatzy_player_total_score(game->players[i]));
+    mvwaddnstr(win, y, 9, list[i].name, 28);
+    mvwprintw(win, y, 38, "%3d", list[i].score);
   }
 
   wgetch(win);
